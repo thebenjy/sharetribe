f21f9a2cba5c8fd156c423fdddf4582a07f34bfa
Ben Forrest <ben.forrest@gmail.com>
2017-09-10 09:52:08 +1000

    try again

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -63,10 +63,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = APP_CONFIG.sharetribe_mail_from_address
-  # config not set early enought
-  # not picking up the normal config!!!?
-#  config.secret_key = APP_CONFIG.secret_key_base 
-  config.secret_key = ENV['SECRET_TOKEN']
+  config.secret_key = APP_CONFIG.secret_key_base 
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
